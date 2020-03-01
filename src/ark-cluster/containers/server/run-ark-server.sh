6c46c286c178ab858a33f1d5dd8ba3ae88d4d34a
Jye Karl-Perry <me@jye.guru>
2020-03-01 13:13:34 +1100

    Main Update - Initial Test

--- src/ark-cluster/containers/server/run-ark-server.sh
+++ src/ark-cluster/containers/server/run-ark-server.sh
@@ -2,7 +2,7 @@
 
 ARKSERVERCOMMAND="/home/steam/ark/ShooterGame/Binaries/Linux/ShooterGameServer"
 ARKSAVEFOLDER="/home/steam/ark/ShooterGame/Saved"
-CONFIGSTRING="\"$MAP?listen?Multihome=0.0.0.0?AltSaveDirectoryName=$MAP?SessionName=$SESSIONNAME?MaxPlayers=$MAXPLAYERS?QueryPort=$QUERYPORT?RCONPort=$RCONPORT?Port=$PORT?ServerAdminPassword=$SERVERADMINPASSWORD\""
+CONFIGSTRING="\"$MAP?listen?Multihome=0.0.0.0?AltSaveDirectoryName=$MAP?SessionName=$SESSIONNAME?MaxPlayers=$MAXPLAYERS?QueryPort=$QUERYPORT?RCONPort=$RCONPORT?Port=$PORT\""
 
 if [ -n "$CLUSTERID" ]; then
     echo "Cluster ID is blank, running as standalone.";
@@ -12,4 +12,12 @@ else
     CLUSTERCONFIG="-NoTransferFromFiltering -clusterid=$CLUSTERID -ClusterDirOverride=\"$ARKSAVEFOLDER/Cluster-$CLUSTERID\""
 fi
 
-$ARKSERVERCOMMAND $CONFIGSTRING $CLUSTERCONFIG -server -log
+if [ -n "$ACTIVEEVENT" ]; then
+    echo "Active Event is blank, running vanilla.";
+    EVENTCONFIG="-ActiveEvent=None"
+else
+    echo "Setting Active Event config to: $ACTIVEEVENT";
+    EVENTCONFIG="-ActiveEvent=$ACTIVEEVENT"
+fi
+
+$ARKSERVERCOMMAND $CONFIGSTRING $EVENTCONFIG $CLUSTERCONFIG -server -log
